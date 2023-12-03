399cfb4ce692d40210a50b22eee4fb8fbfc2ccd0
Waldir Júnior <waldirjunior.gpn@gmail.com>
2023-12-03 10:03:21 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/WaldirJunior/UolHost/UolHostApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.WaldirJunior.UolHost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
